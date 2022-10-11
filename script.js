d9a9a50aa3c1f369cd14d2fd446f94d5ac9f52b9
geoffont <geoffroy.font@gmail.com>
2022-10-11 21:06:13 +0200

    finalisation slider

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
-let img_slider = document.getElementsByClassName("img_slider");
+let img__slider = document.getElementsByClassName('img__slider');
 
 let etape = 0;
 
-let nbr_img = img_slider.length;
+let nbr__img = img__slider.length;
 
-let retour = document.querySelector(".retour");
-let suivant = document.querySelector(".suivant");
+let precedent = document.querySelector('.precedent');
+let suivant = document.querySelector('.suivant');
 
 function enleverActiveImages() {
-	for (let i = 0; i < nbr_img; i++) {
-		img_slider[i].classList.remove("active");
-	}
+    for(let i = 0 ; i < nbr__img ; i++) {
+        img__slider[i].classList.remove('active');
+    }
 }
 
-suivant.addEventListener("click", function () {
-	etape++;
-	if (etape >= nbr_img) {
-		etape = 0;
-	}
-	enleverActiveImages();
-	img_slider[etape].classList.add("active");
-});
+suivant.addEventListener('click', function() {
+    etape++;
+    if(etape >= nbr__img) {
+        etape = 0;
+    }
+    enleverActiveImages();
+    img__slider[etape].classList.add('active');
+})
 
-retour.addEventListener("click", function () {
-	etape--;
-	if (etape < 0) {
-		etape = nbr_img - 1;
-	}
-	enleverActiveImages();
-	img_slider[etape].classList.add("active");
-});
+precedent.addEventListener('click', function() {
+    etape--;
+    if(etape < 0) {
+        etape = nbr__img - 1;
+    }
+    enleverActiveImages();
+    img__slider[etape].classList.add('active');
+})
+
+setInterval(function() {
+    etape++;
+    if(etape >= nbr__img) {
+        etape = 0;
+    }
+    enleverActiveImages();
+    img__slider[etape].classList.add('active');
+}, 3000)
