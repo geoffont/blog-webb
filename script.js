a6a2558fc9de890250c08d7e0b30d4c2e46f7642
geoffont <geoffroy.font@gmail.com>
2022-10-16 20:33:34 +0200

    release update final version

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*-------------------------------------------fonction slide--------------------------------*/
 let img__slider = document.getElementsByClassName("img__slider");
 
 let etape = 0;
@@ -8,79 +9,53 @@ let precedent = document.querySelector(".precedent");
 let suivant = document.querySelector(".suivant");
 
 function enleverActiveImages() {
-	for (let i = 0; i < nbr__img; i++) {
-		img__slider[i].classList.remove("active");
-	}
+  for (let i = 0; i < nbr__img; i++) {
+    img__slider[i].classList.remove("active");
+  }
 }
 
 suivant.addEventListener("click", function () {
-	etape++;
-	if (etape >= nbr__img) {
-		etape = 0;
-	}
-	enleverActiveImages();
-	img__slider[etape].classList.add("active");
+  etape++;
+  if (etape >= nbr__img) {
+    etape = 0;
+  }
+  enleverActiveImages();
+  img__slider[etape].classList.add("active");
 });
 
 precedent.addEventListener("click", function () {
-	etape--;
-	if (etape < 0) {
-		etape = nbr__img - 1;
-	}
-	enleverActiveImages();
-	img__slider[etape].classList.add("active");
+  etape--;
+  if (etape < 0) {
+    etape = nbr__img - 1;
+  }
+  enleverActiveImages();
+  img__slider[etape].classList.add("active");
 });
-
+/*------------------------------fonction formulaire---------------------*/
 const message =
-	"Merci pour votre retour! voici un petit cadeaux en mode display ;)";
+  ", merci pour votre retour! voici un petit cadeaux en mode display ;)";
 
 let systeme_solaire = document.getElementById("systeme_solaire");
 
 document
-	.getElementById("formulaire")
-	.addEventListener("submit", function (event) {
-		let inpuText = document.querySelector(".inpuText").value;
-		event.preventDefault();
-
-		alert(inpuText + " " + message);
-
-		if (getComputedStyle(systeme_solaire).display != "none") {
-			systeme_solaire.style.display = "none";
-		}
-
-		if (window.innerWidth < 600) {
-			$("systeme_solaire").hide(systeme_solaire);
-		} else {
-			systeme_solaire.style.display = "block";
-		}
-	});
-
+  .getElementById("formulaire")
+  .addEventListener("submit", function (event) {
+    let inpuText = document.querySelector(".inpuText").value;
+    event.preventDefault();
+    alert(inpuText + " " + message);
+    if (getComputedStyle(systeme_solaire).display != "none") {
+      systeme_solaire.style.display = "none";
+    }
+    if (window.innerWidth < 600) {
+      $("systeme_solaire").hide(systeme_solaire);
+    } else {
+      systeme_solaire.style.display = "block";
+    }
+  });
+/*----------------------fonction scroll-------------------------*/
 window.addEventListener("scroll", function () {
-	const distance = window.scrollY;
-	document.querySelector(".gal-a").style.transform = `translateY(${
-		distance * -0.1
-	}px)`;
-	document.querySelector(".gal-b").style.transform = `translateY(${
-		distance * -0.1
-	}px)`;
-	document.querySelector(".gal-c").style.transform = `translateY(${
-		distance * -0.1
-	}px)`;
-	document.querySelector(".gal-d").style.transform = `translateY(${
-		distance * -0.1
-	}px)`;
-	document.querySelector(".gal-e").style.transform = `translateY(${
-		distance * -0.1
-	}px)`;
-	document.querySelector(".gal-f").style.transform = `translateY(${
-		distance * -0.1
-	}px)`;
-	document.querySelector("#formulaire").style.transform = `translateY(${
-		distance * -0.1
-	}px)`;
-	document.querySelector("#script_solaire").style.transform = `translateY(${
-		distance * -0.1
-	}px)`;
-  
-  
+  const distance = window.scrollY;
+  document.querySelector("body").style.transform = `translateY(${
+    distance * -0.15
+  }px)`;
 });
